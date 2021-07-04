813030a971296a87b3bd52aa24ed4e0bc37c6a09
Gaurav <mishragaurav656@gmail.com>
2021-07-04 18:44:38 +0530

    added port in index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./server");
 const socket_config = require("./config/socket.io/socket");
-PORT = 4000;
+PORT = process.env.PORT || 4000;
 const server = app.listen(PORT, (err) => {
   if (err) console.log(err);
   console.log(`listening on the port ${PORT}`);
